417cac8abe71c552b491a48a831b3794e37a8e96
AlisaLi1981 <124600383+AlisaLi1981@users.noreply.github.com>
2023-10-19 19:19:49 +0300

    Update effective_quicksort.py

--- effective_quicksort.py
+++ effective_quicksort.py
@@ -1,36 +1,43 @@
-# ID 93703369
+# ID 93774488
 
 import random
 
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+class Competitor:
+    def __init__(self, login: str, tasks_solved: int, penalty: int):
+        self.login = login
+        self.tasks_solved = tasks_solved
+        self.penalty = penalty
 
-    pivot_idx = random.randint(0, len(arr)-1)
+    def __lt__(self, other: 'Competitor') -> bool:
+        if isinstance(other, Competitor):
+            return (
+                (-self.tasks_solved, self.penalty, self.login) <=
+                (-other.tasks_solved, other.penalty, other.login)
+            )
+
+    def __str__(self) -> str:
+        return self.login
+
+
+def quick_sort(arr: list, start: int, end: int) -> None:
+    if start >= end:
+        return
+
+    pivot_idx = random.randint(start, end)
     pivot = arr[pivot_idx]
-    arr[pivot_idx], arr[-1] = arr[-1], arr[pivot_idx]
+    arr[pivot_idx], arr[end] = arr[end], arr[pivot_idx]
 
-    i = 0
-    for j in range(len(arr)-1):
-        if compare(arr[j], pivot) < 0:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
+    left = start
+    for right in range(start, end):
+        if arr[right] < pivot:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
 
-    arr[i], arr[-1] = arr[-1], arr[i]
+    arr[left], arr[end] = arr[end], arr[left]
 
-    return quick_sort(arr[:i]) + [arr[i]] + quick_sort(arr[i+1:])
-
-
-def compare(a, b):
-    login_a, tasks_solved_a, penalty_a = a
-    login_b, tasks_solved_b, penalty_b = b
-
-    if tasks_solved_a == tasks_solved_b:
-        if penalty_a == penalty_b:
-            return -1 if login_a < login_b else 1
-        return -1 if penalty_a < penalty_b else 1
-    return -1 if tasks_solved_a > tasks_solved_b else 1
+    quick_sort(arr, start, left-1)
+    quick_sort(arr, left+1, end)
 
 
 if __name__ == '__main__':
@@ -38,7 +45,9 @@ if __name__ == '__main__':
     participants = []
     for _ in range(n):
         login, tasks_solved, penalty = input().split()
-        participants.append((login, int(tasks_solved), int(penalty)))
+        participants.append(Competitor(login, int(tasks_solved), int(penalty)))
 
-    for login, _, _ in quick_sort(participants):
-        print(login)
+    quick_sort(participants, 0, n-1)
+
+    for competitor in participants:
+        print(competitor)
